3862695ef1a682d50e3cefa4bcac9f3f6a421859
ctturuk <ctturuk@gmail.com>
2021-03-04 20:55:58 -0700

    revised Javascript

--- main.js
+++ main.js
@@ -4,40 +4,15 @@ const adj = ['tasty', 'fluffy', 'slick', 'filthy', 'uneven', 'warm', 'faint', 'm
 const verb = ['cling', 'awaken', 'grow', 'run', 'froze', 'paid', 'shook', 'flew', 'fell', 'drown'];
 const adverb = ['highly', 'honestly', 'nearly', 'roughly', 'rudely', 'scarcely', 'madly', 'adversely', 'rightfully', 'jealously'];
 
-// function to determine 3 random nouns
-function assignNoun(n) {
-    let randNoun = [];
-    for (let i = 0; i < n; i++) {
-        randNoun.push(noun.splice(Math.random()*(noun.length-1), 1).pop())
+// function to randomize array
+const shuffle = (array, n) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-    return randNoun
-}
-
-// function to determine 4 random adjectives
-function assignAdj(n) {
-    let randAdj = [];
-    for (let i = 0; i < n; i++) {
-        randAdj.push(adj.splice(Math.random()*(noun.length-1), 1).pop())
-    }
-    return randAdj
-}
-
-// function to determine 3 random verbs
-function assignVerb(n) {
-    let randVerb = [];
-    for (let i = 0; i < n; i++) {
-        randVerb.push(verb.splice(Math.random()*(noun.length-1), 1).pop())
-    }
-    return randVerb
-}
-
-// function to determine 2 random adverbs
-function assignAdverb(n) {
-    let randAdverb = [];
-    for (let i = 0; i < 2; i++) {
-        randAdverb.push(adverb.splice(Math.random()*(noun.length-1), 1).pop())
-    }
-    return randAdverb
+    return array;
 }
 
 // Create an array based on classes of the text
@@ -50,28 +25,28 @@ let writeText = () => {
 
         if (classes[j] === 'adj') {
 
-            let selectAdj = assignAdj(elements.length); 
+            let selectAdj = shuffle(adj, (elements.length)); 
 
             for(let y=0; y<elements.length; y++) {
                 elements[y].innerHTML = selectAdj[y];
             }              
         } else if (classes[j] === 'noun') {
             
-            let selectNoun = assignNoun(elements.length); 
+            let selectNoun = shuffle(noun, (elements.length)); 
 
             for(let y=0; y<elements.length; y++) {
                 elements[y].innerHTML = selectNoun[y];
             }  
         } else if (classes[j] === 'adverb') {
 
-            let selectAdverb = assignAdverb(elements.length); 
+            let selectAdverb = shuffle(adverb, (elements.length)); 
 
             for(let y=0; y<elements.length; y++) {
                 elements[y].innerHTML = selectAdverb[y];
             }  
         } else if (classes[j] === 'verb'){
             
-            let selectVerb = assignVerb(elements.length); 
+            let selectVerb = shuffle(verb, (elements.length)); 
             
             for(let y=0; y<elements.length; y++) {
                 elements[y].innerHTML = selectVerb[y];
@@ -82,4 +57,3 @@ let writeText = () => {
 }
 
 document.getElementById("generate").addEventListener('click', writeText);
-
